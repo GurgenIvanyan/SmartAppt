ca85ac2a6762fc87736a2a073f39825e6fc3f1e4
GurgenIvanyan <gurgenivanyan9812@icloud.com>
2025-12-07 19:02:06 +0400

    Log Commit For Front Final

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -4,22 +4,21 @@ using Common.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1) Քո custom logging-ի կոնֆիգուրացիան (UI-ի համար էլ)
+
 AppLoggerFactory.Configure(options =>
 {
     options
-        .AddSink(new DebugLog()) // VS Output window
+        .AddSink(new DebugLog()) 
         .AddSink(new FileLog(
-            // Կարաս պապկան փոխես, եթե ուզում ես
+     
             directory: Path.Combine(AppContext.BaseDirectory, "logs")))
-        // Որ level-ից վերև գրի
+      
         .SetFilter(e => e.Level >= Common.Logging.LogLevel.Info);
 });
 
-// 2) Razor Pages
+
 builder.Services.AddRazorPages();
 
-// 3) HttpClient դեպի API
 builder.Services.AddHttpClient("SmartApptApi", client =>
 {
     client.BaseAddress = new Uri("http://localhost:5213/");
@@ -27,7 +26,7 @@ builder.Services.AddHttpClient("SmartApptApi", client =>
 
 var app = builder.Build();
 
-// Փոքր стартовый log
+
 var startupLog = AppLoggerFactory.CreateLogger("Startup");
 startupLog.Info("Web UI started");
 
